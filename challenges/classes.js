f6caae66593f20ea0ff42f3cd2ee93a2dcc82ce0
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-03-24 05:04:04 +0430

    changes

--- challenges/classes.js
+++ challenges/classes.js
@@ -18,7 +18,7 @@ class CuboidMaker {
           super (obj);
           this.color = obj.color;
         }
-      }
+      };
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
